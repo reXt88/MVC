a604c866a8b8d2f822fb09e9f3b70c2169dcdeeb
alexlina4002@gmail.com <alexlina4002@gmail.com>
2022-12-09 12:20:35 +0300

    Добавьте файлы проекта.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index ()
+        {
+            return View();
+        }
+        //public string Welcome(string name, int numTimes = 1) 
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            //return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
